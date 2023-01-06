55f5da452bfbd0656ad96cc50f4fa97367981783
Mizter Egoist <mizteregoist@gmail.com>
2023-01-06 13:12:46 +0300

    Initial commit

--- docker/docker_entrypoint_prod.sh
+++ docker/docker_entrypoint_prod.sh
@@ -9,6 +9,6 @@ python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
 
-gunicorn app.wsgi:application --bind 0.0.0.0:8000
+daphne app.asgi:application --bind 0.0.0.0 --port 8000
 
 exec "$@"
